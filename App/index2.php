c30b4f58ae6262e5246b76a216a6c9acca1893c9
D . L . Eranda Malshika <106301031+EMalshika@users.noreply.github.com>
2024-08-07 00:38:26 +0530

    17

---
+++ App/index2.php
@@ -0,0 +1,338 @@
+<?php
+    include "connection.php";
+    $price=0;
+?>
+<!DOCTYPE html>
+<html lang="en" class=" ">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <title>RJAY Tours</title>
+  <meta content="RJAY Tours Mobile App" name="description" />
+  <meta content="themepassion" name="author" />
+
+
+  <!-- App Icons -->
+  <!--<link rel="apple-touch-icon" sizes="57x57" href="assets/images/icons/apple-icon-57x57.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="assets/images/icons/apple-icon-60x60.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="assets/images/icons/apple-icon-72x72.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/images/icons/apple-icon-76x76.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="assets/images/icons/apple-icon-114x114.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="assets/images/icons/apple-icon-120x120.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="assets/images/icons/apple-icon-144x144.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="assets/images/icons/apple-icon-152x152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/images/icons/apple-icon-180x180.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="assets/images/icons/android-icon-192x192.png">
+  <link rel="icon" type="image/png" sizes="512x512" href="assets/images/icons/android-icon-512x512.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="assets/images/icons/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="assets/images/icons/favicon-96x96.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="assets/images/icons/favicon-16x16.png">-->
+  <link rel="manifest" href="assets/images/icons/manifest.json">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="msapplication-TileImage" content="assets/images/icons/ms-icon-144x144.png">
+  <meta name="theme-color" content="#ffffff">
+
+
+
+
+
+
+  <!-- CORE CSS FRAMEWORK - START -->
+  <link href="assets/css/preloader.css" type="text/css" rel="stylesheet" media="screen" />
+
+  <link href="modules/materialize/materialize.min.css" type="text/css" rel="stylesheet" media="screen" />
+  <link href="modules/fonts/mdi/appicon/appicon.css" type="text/css" rel="stylesheet" media="screen" />
+  <link href="modules/fonts/mdi/materialdesignicons.min.css" type="text/css" rel="stylesheet" media="screen" />
+  <link href="modules/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen" />
+
+
+  <!-- CORE CSS FRAMEWORK - END -->
+
+  <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START -->
+  <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - END -->
+
+  <!-- CORE CSS TEMPLATE - START -->
+
+
+  <link href="assets/css/style.css" type="text/css" rel="stylesheet" media="screen" id="main-style" />
+  <!-- CORE CSS TEMPLATE - END -->
+
+
+
+</head>
+<!-- END HEAD -->
+
+<!-- BEGIN BODY -->
+
+
+<body class="   html" data-header="light" data-footer="light" data-header_align="app" data-menu_type="left"
+  data-menu="light" data-menu_icons="on" data-footer_type="left" data-site_mode="light" data-footer_menu="show"
+  data-footer_menu_style="light">
+  <div class="preloader-background">
+    <div class="preloader-wrapper">
+      <div class="orig">
+
+        <div class="dot orig three "></div>
+        <div class="dot orig two "></div>
+        <div class="dot orig one "></div>
+      </div>
+
+    </div>
+  </div>
+
+
+
+  <!-- SIDEBAR - START -->
+
+  <!-- MAIN MENU - START -->
+
+
+
+  <!-- MAIN MENU - END -->
+
+
+
+  <!--  SIDEBAR - END --><!-- SIDEBAR - START -->
+
+  <!-- MAIN MENU - START -->
+
+
+
+  <!-- MAIN MENU - END -->
+
+
+
+  <!--  SIDEBAR - END -->
+
+  
+  
+
+
+  
+  <div class="menu-close"><i class="mdi mdi-close"></i></div>
+
+  <div class="content-area">
+
+
+    <div class="container">
+      <div class="section ui-select">
+
+
+        <div class="row ">
+          <div class="col s12 pad-0">
+
+
+
+            <form action="" method="post">
+              <!-- select multiple -->
+              <div class="">
+
+                <div class="input-field sel-wrap sel-wrap col s12">
+                  <select name="start">
+                    <option>Choose Your Location</option>
+                    <option>Ahangama</option>
+                    <option>Anuradhapura</option>
+                    <option>Arugambay</option>
+                    <option>Benthota</option>
+                    <option>BIA</option>
+                    <option>Colombo</option>
+                    <option>Dambulla</option>
+                    <option>Dikwella</option>
+                    <option>Ella</option>
+                    <option>Galle</option>
+                    <option>Hikkaduwa</option>
+                    <option>Hiriketiya</option>
+                    <option>Kandy</option>
+                    <option>Mirissa</option>
+                    <option>Negambo</option>
+                    <option>Sigiriya</option>
+                    <option>Tangalle</option>
+                    <option>Trincomalee</option>
+                    <option>Udawalawa</option>
+                    <option>Unawatuna</option>
+                    <option>Weligama</option>
+                    <option>Yala</option>
+                  </select>
+                  <label>Select Start Location</label>
+                </div>
+
+
+
+              </div>
+
+
+              <!-- select with optgroups -->
+              <div class="">
+                <div class="input-field sel-wrap col s12">
+                  <select name="end">
+                    <option>Choose Your End Location</option>
+                    <option>Ahangama</option>
+                    <option>Anuradhapura</option>
+                    <option>Arugambay</option>
+                    <option>Benthota</option>
+                    <option>BIA</option>
+                    <option>Colombo</option>
+                    <option>Dambulla</option>
+                    <option>Dikwella</option>
+                    <option>Ella</option>
+                    <option>Galle</option>
+                    <option>Hikkaduwa</option>
+                    <option>Hiriketiya</option>
+                    <option>Kandy</option>
+                    <option>Mirissa</option>
+                    <option>Negambo</option>
+                    <option>Sigiriya</option>
+                    <option>Tangalle</option>
+                    <option>Trincomalee</option>
+                    <option>Udawalawa</option>
+                    <option>Unawatuna</option>
+                    <option>Weligama</option>
+                    <option>Yala</option>
+                  </select>
+                  <label>Select End Location</label>
+                </div>
+
+                <!-- select with optgroups -->
+                <div class="input-field sel-wrap col s12">
+                  <select name="vehicle">
+                    <option>Choose Vehicle</option>
+                    <option>Flex car</option>
+                    <option>Sedan</option>
+                    <option>Van</option>
+                  </select>
+                  <label>Select Vehicle</label>
+                </div>
+
+              </div>
+
+              <button type="submit" name="submit1" class="btn btn-success">Calculate</button>
+            </form>
+
+            <div class="pad-15">
+
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="ndistance" type="text" class="validate">
+                  <label for="first_name1">Distance</label>
+                </div>
+              </div>
+
+              <div class="row">
+                <div class="input-field col s12">
+                  <input id="nprice" type="text" class="validate">
+                  <label for="first_name1">Price</label>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+
+
+<?php
+  if (isset($_POST["submit1"]))
+  {
+    $startlocation = $_POST["start"];
+    $endlocation = $_POST["end"];
+    $vehicle = $_POST["vehicle"];
+
+    /*?>
+    <script>alert("<?php echo $vehicle; ?>");</script>
+    <?php*/
+
+    $res = mysqli_query($link, "select * from distance where start='$startlocation' && end='$endlocation'");
+    while ($row = mysqli_fetch_array($res)) {
+      $distance = $row["distance"];
+    }
+
+    if ($vehicle == "Flex car") {
+      $price = $distance * 125;
+    }elseif ($vehicle == "Sedan") {
+      $price = $distance * 150;
+    }elseif ($vehicle == "Van") {
+      $price = $distance * 200;
+    }
+
+    /*?>
+    <script>alert("<?php echo $price; ?>");</script>
+    <?php*/
+
+    ?>
+    <script>
+    document.getElementById("ndistance").value = "<?php echo $distance.' '.'Km'; ?>";
+    document.getElementById("nprice").value = "<?php echo 'Rs.'.' '. $price.' '.'/='; ?>";
+    </script>
+    <?php
+
+    /*?>
+    <script>alert("<?php echo $price; ?>");</script>
+    <?php*/
+
+
+
+  }
+?>
+
+
+
+    
+
+
+
+    <div class="backtotop">
+      <a class="btn-floating btn primary-bg">
+        <i class="mdi mdi-chevron-up"></i>
+      </a>
+    </div>
+
+
+
+  </div><!--.content-area-->
+
+  
+
+
+
+
+
+
+
+
+  <script src="assets/js/pwa.js"></script>
+
+  <!-- LOAD FILES AT PAGE END FOR FASTER LOADING -->
+
+  <!-- CORE JS FRAMEWORK - START -->
+  <script src="modules/jquery/jquery-2.2.4.min.js"></script>
+  <script src="modules/materialize/materialize.js"></script>
+  <script src="modules/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+  <script src="assets/js/variables.js"></script>
+  
+  <!-- CORE JS FRAMEWORK - END -->
+
+
+  <!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - START -->
+  <script type="text/javascript">
+    $("select").formSelect();
+  </script>
+  <script src="assets/js/common.js"></script><!-- OTHER SCRIPTS INCLUDED ON THIS PAGE - END -->
+
+
+  <!-- CORE TEMPLATE JS - START -->
+  <script src="modules/app/init.js"></script>
+  <script src="modules/app/settings.js"></script>
+
+  <script src="modules/app/scripts.js"></script>
+
+  <!-- END CORE TEMPLATE JS - END -->
+
+
+  <script src="assets/js/preloader.js"></script>
+</body>
+
+</html>
